c2b887838b6068e1eb6de7f6024baa583f7146e4
Jeff Hann <jeffhann@gmail.com>
2014-12-08 14:55:40 -0400

    fixing delete function

--- queue/main.go
+++ queue/main.go
@@ -13,23 +13,15 @@ func main() {
 		fmt.Printf("Adding %v\n", (x + 1))
 
 		queue.Add(x + 1)
-
-		fmt.Printf("End is %v\n", queue.End())
-		fmt.Printf("Len is %v\n", queue.Len())
-		fmt.Printf("Cap %v\n\n", queue.Cap())
 	}
 
-	fmt.Printf("Deleted %v\n", queue.Del())
+	for x := 0; x < size; x++ {
+		num := queue.Del()
 
-/*
- *    for x := 0; x < size; x++ {
- *        num := queue.Del()
- *
- *        if num == -1 {
- *            fmt.Printf("Del failed! Queue is empty.")
- *        } else {
- *            fmt.Printf("Deleted %v\n", num)
- *        }
- */
+		if num == -1 {
+			fmt.Printf("Del failed! Queue is empty.")
+		} else {
+			fmt.Printf("Deleted %v\n", num)
+		}
 	}
 }
